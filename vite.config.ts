c2cbfd01ed27053a92d9c1dd30f561a4c34c20f7
SafaElmali <tsafaelmali@gmail.com>
2022-02-15 18:23:49 +0300

    Add vite path alias config
    Closes #18

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import macrosPlugin from "vite-plugin-babel-macros";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import macrosPlugin from 'vite-plugin-babel-macros';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), macrosPlugin()],
   resolve: {
     alias: {
-      components: path.resolve(__dirname, "./src/components"),
-      config: path.resolve(__dirname, "./src/config"),
-      "styled-configs": path.resolve(__dirname, "./src/styled-configs"),
+      components: path.resolve(__dirname, './src/components'),
+      config: path.resolve(__dirname, './src/config'),
+      'styled-configs': path.resolve(__dirname, './src/styled-configs'),
     },
   },
 });
