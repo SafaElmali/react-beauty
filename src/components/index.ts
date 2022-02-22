83d1f85a5e08a98d7e8c1561cb8e2483295c9afc
SafaElmali <tsafaelmali@gmail.com>
2022-02-22 15:05:34 +0300

    Add export components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Box } from 'components/Layouts/Box/Box';
+export { default as Center } from 'components/Layouts/Center/Center';
+export { default as Flex } from 'components/Layouts/Flex/Flex';
+export { default as Grid } from 'components/Layouts/Grid/Grid';
+export { default as Provider } from 'components/Provider/BeautyProvider';
